df5ea7e7f48a2e3936bd2bc7f9f0b23917fd6642
Naxelsson <87854878+Naxelsson@users.noreply.github.com>
2021-07-23 12:05:49 +0200

    Add files via upload

---
+++ sod_test.py
@@ -0,0 +1,82 @@
+
+import matplotlib.pyplot as plt
+
+from datetime import timedelta, date
+# https://www.w3resource.com/python-exercises/date-time-exercise/python-date-time-exercise-50.php
+
+
+fran = input("Ange datum du vill läsa från: yyyy-mm-dd ")
+till = input("Ange datum du vill läsa till: yyyy-mm-dd ")
+#fran = "1950-01-01"
+#till = "1950-12-31"
+    
+fran2 = fran.replace('-',' ')
+till2 = till.replace('-',' ')
+    
+fran2 = fran2.split()
+till2 = till2.split()
+        
+
+#########################################
+def daterange(date1, date2):
+    for n in range(int ((date2 - date1).days)+1):
+        yield date1 + timedelta(n)
+    
+datList = []
+#start_dt = date(2015, 12, 20)
+#end_dt = date(2016, 1, 11)
+start_dt = date(int(fran2[0]), int(fran2[1]), int(fran2[2]))
+end_dt = date(int(till2[0]), int(till2[1]), int(till2[2]))
+for dt in daterange(start_dt, end_dt):
+    datList.append(dt.strftime("%Y-%m-%d"))
+        #print(dt.strftime("%Y-%m-%d"))
+#print(datList)
+############
+    
+temper_list = []
+    
+def ladda_csv(filename):    # Skapar funktionen
+    import csv
+    
+    with open(filename, 'r') as csvFile: # öppnar filen
+        reader = csv.reader(csvFile)
+
+        for row in reader:
+            row_str = ''.join(row)
+            current_row = row_str.split(';')
+            if current_row[0] in datList and current_row[1] == '15:00:00':
+                temper_list.append(current_row[2])
+         
+
+# Anropar funktionen ovan så att lexikonen skapas       
+#ladda_csv('testdokumentkod.txt')
+ladda_csv('smhi-opendata_soderhamn_csvfil.csv')
+tempen = [float(i) for i in temper_list]
+
+xx = range(len(datList))
+xx = list(xx)
+plt.plot(xx,tempen)
+plt.xlabel("Dagar")                             # lägger till info till plotten
+plt.ylabel("Temperatur [C]") 
+plt.title("Temperatur vid 15:00 mellan " + fran + " och " + till)
+plt.grid()
+
+
+plt.show()
+
+
+
+
+        
+        
+        
+
+
+            
+            
+            
+#load_csv('CO2Emissions_filtered.csv')  # Anropar funktionen ovan så att lexikonen skapas
+#ladda_csv('smhi-opendata_soderhamn_csvfil.csv')  # Anropar funktionen ovan så att lexikonen skapas
+
+
+
